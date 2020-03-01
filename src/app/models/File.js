ebade37980c6e7bb6569d487b18d31e54819a70b
Alan Cristian <lanzn16@outlook.com>
2020-03-01 20:24:06 -0300

    update avatar url access

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -1,11 +1,19 @@
 import Sequelize, { Model } from "sequelize";
 
+import authConfig from "../../config/auth";
+
 class File extends Model {
     static init(sequelize) {
         super.init(
             {
                 name: Sequelize.STRING,
-                path: Sequelize.STRING
+                path: Sequelize.STRING,
+                url: {
+                    type: Sequelize.VIRTUAL,
+                    get() {
+                        return `${authConfig.address_acess}/files/${this.path}`;
+                    }
+                }
             },
             {
                 sequelize
--- src/config/auth.js
+++ src/config/auth.js
@@ -1,6 +1,7 @@
 require("dotenv/config");
 
 export default {
+    address_acess: process.env.ACCESS_ADDRESS,
     secret: process.env.SECRET,
     expiresIn: process.env.EXPIRES_IN
 };
