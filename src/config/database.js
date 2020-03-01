70228fb56a48145f51fd1f433d00ab8a685d62cd
Alan Cristian <lanzn16@outlook.com>
2020-02-29 21:50:36 -0300

    add config database

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: "postgres",
+    host: "localhost",
+    username: "postgres",
+    password: "admin01",
+    database: "first-beard",
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true
+    }
+};
