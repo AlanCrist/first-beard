978f7a88107df5489fed203bfe7a1541853a3850
Alan Cristian <lanzn16@outlook.com>
2020-03-04 23:11:26 -0300

    add configuration email for appointment

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    secure: false,
+    auth: {
+        user: "d53f144f68e596",
+        pass: "1a5ce0f4dec1c0"
+    },
+    default: {
+        from: "Equipe first-beard <noreply@first-beard.com>"
+    }
+};
